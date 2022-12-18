ddb2d24059ff72fd2194028232f89e7acd6c4ba4
Ilshat <illliat@mail.ru>
2022-12-18 19:42:54 +0300

    Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Методы.
+int[] EnterNums(int length)
+{
+    Console.WriteLine($"Введите {length} чисел");
+    int[] numsArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        int number = int.Parse(Console.ReadLine());
+        numsArray[i] = number;
+    }
+    return numsArray;
+}
+
+int CountingPositivNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+// Создание массива из М чисел.
+Console.Write("Введите число: ");
+int length = int.Parse(Console.ReadLine());
+int[] array = EnterNums(length);
+// Подсчет количества чисел больше 0.
+int result = CountingPositivNumbers(array);
+// Вывод результата.
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"{result} чисел больше 0");
